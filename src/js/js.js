0234ccbc0273763d37ff4a5049c6df0bbb79481e
Bogdan <musnikbogda3@gmail.com>
2025-05-07 14:18:54 +0500

    Сделали модальное окно

--- src/js/js.js
+++ src/js/js.js
@@ -50,7 +50,8 @@ $(document).ready(function () {
         .removeClass("catalog__content_active")
         .eq($(this).index())
         .addClass("catalog__content_active");
-    });
+    }
+  );
 
   function toggleSlide(item) {
     $(item).each(function (i) {
@@ -62,7 +63,24 @@ $(document).ready(function () {
         $(".catalog-item__list").eq(i).toggleClass("catalog-item__list_active");
       });
     });
-  };
+  }
   toggleSlide(".catalog-item__link");
   toggleSlide(".catalog-item__back");
+
+  /////////////////////////////////modal
+
+  $("[data-modal=consultation]").on("click", function () {
+    $(".overlay, #consultation").fadeIn("slow");
+  });
+
+  $(".modal__close").on("click", function () {
+    $(".overlay, #consultation, #order, #thank").fadeOut("slow");
+  });
+
+  $(".button_mini").each(function (i) {
+    $(this).on("click", function () {
+      $("#order .modal__desr").text($(".catalog-item__subtitle").eq(i).text());
+      $(".overlay, #order").fadeIn("slow");
+    });
+  });
 });
