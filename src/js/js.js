84a678985320a256e8a8a4a8134349298706225c
Bogdan <musnikbogda3@gmail.com>
2025-08-13 20:31:26 +0500

    Валидация форм

--- src/js/js.js
+++ src/js/js.js
@@ -83,4 +83,29 @@ $(document).ready(function () {
       $(".overlay, #order").fadeIn("slow");
     });
   });
+
+  function validateForms(form) {
+    $(form).validate({
+      rules: {
+        name: "required",
+        phone: "required",
+        email: {
+          required: true,
+          email: true,
+        },
+      },
+      messages: {
+        name: "Введи имя ебанат",
+        phone: "Введи номер дура",
+        email: {
+          required: "Почта дура",
+          email: "Введи верно ее дура",
+        },
+      },
+    });
+  }
+  validateForms("#consultation-form");
+  validateForms("#consultation form");
+  validateForms("#order form");
+  $("input[name=phone]").mask("+7 (999) 999-999");
 });
